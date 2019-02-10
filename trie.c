4f1873e93a4773d90c708bcd92341147978c8432
Nicholas Helke <nhelke@gmail.com>
2019-02-10 18:20:43 +0000

    Pack child list and use bsearch over it, instead of direct lookup, this does not alter the time-complexity of the trie as we will bsearch over at most 256 elements, a constant

--- trie.c
+++ trie.c
@@ -7,21 +7,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct trie_flags {
-   bool prefix_owned : 1;
-};
-
 struct trie {
-   struct trie_flags flags;
    char *prefix;
    void *value;
-   struct trie *children[255];
+   unsigned char nchildren;
+   struct trie *children;
 };
+
+static
+void
+trie_init(struct trie *trie)
+{
+   memset(trie, 0, sizeof(struct trie));
+}
 
 struct trie *
 trie_create(void)
 {
-   return calloc(1, sizeof(struct trie));
+   struct trie *trie = malloc(sizeof(struct trie));
+   trie_init(trie);
+   return trie;
 }
 
 void
@@ -31,26 +36,57 @@ trie_destroy(struct trie * const trie)
       return;
    }
 
-   if (trie->flags.prefix_owned)
+   if (trie->prefix)
       free(trie->prefix);
 
-   for (unsigned int i = 0; i < 255; i++) {
-      trie_destroy(trie->children[i]);
-   }
+   if (trie->children)
+      free(trie->children);
 
    free(trie);
 }
 
 static
-bool
-has_children(struct trie * const trie)
+void
+trie_print(const struct trie * const trie,
+           const int level)
 {
-   for (unsigned int i = 0; i < 255; ++i)
+   for (int i = 0; i < level; ++i)
+      printf("%s", "--");
+   printf("%s => %p\n", trie->prefix, trie->value);
+   for (int i = 0; i < trie->nchildren; ++i)
+      trie_print(&trie->children[i], level+1);
+}
+
+static
+int
+trie_cmp(const char *key,
+         const struct trie *trie)
+{
+   char *prefix = "";
+   if (trie && trie->prefix)
+      prefix = trie->prefix;
+   return key[0] > prefix[0] ? 1 : (key[0] == prefix[0] ? 0 : -1);
+}
+
+static
+bool
+trie_bsearch(const char *key,
+             const struct trie *list,
+             const size_t len,
+             size_t * const index)
+{
+   size_t s = 0, e = len, c;
+   int cmp;
+   while (c = (s+e)/2, cmp = trie_cmp(key, &list[c]), cmp && c > 0 && c < e)
    {
-      if (trie->children[i])
-         return true;
+      if (cmp > 0)
+         s = c + 1;
+      else
+         e = c;
    }
-   return false;
+   if (index)
+      *index = c;
+   return cmp == 0;
 }
 
 bool
@@ -58,59 +94,71 @@ trie_insert(struct trie * const trie,
             const char * const name,
             void * const value)
 {
+   printf("insert %s => %p\n", name, value);
+   trie_print(trie, 0);
    if (!value)
       return false;
-
-   struct trie *node = trie;
 
    if (!name)
       return false;
 
-   for (const char *c = name; *c; ++c)
+   if (!trie->prefix)
    {
-      if (!node->value && !has_children(node))
-      {
-         node->prefix = strcpy(calloc(strlen(c)+1, sizeof(char)), c);
-         node->flags.prefix_owned = true;
+      printf("replace root\n");
+      trie->prefix = strcpy(calloc(strlen(name)+1, sizeof(char)), name);
+      trie->value = value;
+      return true;
+   }
+
+   struct trie *node = trie;
+
+   for (const char *c = name; *c;)
+   {
+      const char *d = node->prefix;
+      for (; *d && *c && *d == *c; ++d, ++c);
+      if (*d) {
+         // we didn't reach the end of node->prefix
+         // we must split
+         struct trie *newchild = trie_create();
+         newchild->prefix = strcpy(calloc(strlen(d)+1, sizeof(char)), d);
+         newchild->value = node->value;
+         newchild->nchildren = node->nchildren;
+         newchild->children = node->children;
+
+         size_t prefixlen = d-node->prefix;
+         node->prefix = realloc(node->prefix, prefixlen+1);
+         node->prefix[prefixlen] = 0;
+         node->value = NULL;
+         node->nchildren = 1;
+         node->children = newchild;
+      }
+      if (!*c) {
+         // we reached the end of node->prefix and the end of name, the current node is the one we were looking for
+         // replace/update the value
          break;
       }
 
-      if (node->prefix && node->prefix[0])
+      size_t index;
+      const size_t len = node->nchildren;
+      if (!trie_bsearch(c, node->children, len, &index))
       {
-         const char *d = node->prefix;
-         for (; *d && *c && *d == *c; ++d, ++c);
-         if (*d) {
-            // we didn't reach the end of node->prefix
-            // we must split
-            struct trie *newchild = trie_create();
-            newchild->prefix = (char *)d+1;
-            newchild->value = node->value;
-            memcpy(newchild->children, node->children, sizeof(newchild->children));
+         ++node->nchildren;
+         node->children = realloc(node->children, (node->nchildren)*sizeof(struct trie));
+         if (index < len)
+            memmove(&node->children[index+1], &node->children[index], (len-index)*sizeof(struct trie));
 
-            node->value = NULL;
-            memset(node->children, 0, sizeof(node->children));
-            node->children[(*d)-1] = newchild;
-
-            // Truncate the prefix after using d to assign newchild to the correct edge above
-            size_t prefixlen = d-node->prefix;
-            node->prefix[prefixlen] = 0;
-         }
-         if (!*c) {
-            // we reached the end of node->prefix and the end of name, the current node is the one we were looking for
-            // replace/update the value
-            break;
-         }
+         trie_init(&node->children[index]);
+         node->children[index].prefix = strcpy(calloc(strlen(c)+1, sizeof(char)), c);
+         node = &node->children[index];
+         break;
       }
-
-      if (!node->children[(*c)-1])
-      {
-         node->children[(*c)-1] = trie_create();
-      }
-
-      node = node->children[(*c)-1];
+      node = &node->children[index];
    }
 
    node->value = value;
+
+   printf("inserted %s => %p\n", name, value);
+   trie_print(trie, 0);
    return true;
 }
 
@@ -124,7 +172,7 @@ trie_fetch(const struct trie * const trie,
    if (!name)
       return false;
 
-   for (const char *c = name; *c; ++c)
+   for (const char *c = name; *c;)
    {
       if (node->prefix && node->prefix[0])
       {
@@ -140,15 +188,14 @@ trie_fetch(const struct trie * const trie,
          }
       }
 
-      if (!node->children[(*c)-1])
+      size_t index;
+      if (!trie_bsearch(c, node->children, node->nchildren, &index))
          return false;
 
-      node = node->children[(*c)-1];
+      node = &node->children[index];
    }
 
-   if (!value)
-      return false;
-
-   *value = node->value;
+   if (value)
+      *value = node->value;
    return true;
 }
