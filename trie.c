10f829887d7f56990c5fb51f76f868e14ea7e0ac
Nicholas Helke <nhelke@gmail.com>
2019-03-16 14:10:56 +0000

    Revert "replace manual binary search with bsearch()"
    This reverts commit d04d87cac64bdd90642110c49efe059964f847e8.

--- trie.c
+++ trie.c
@@ -64,11 +64,9 @@ trie_print(const struct trie * const trie,
 
 static
 int
-trie_cmp(const void *search,
-         const void *element)
+trie_cmp(const char *key,
+         const struct trie *trie)
 {
-   const char *key = search;
-   const struct trie *trie = element;
    char buf[sizeof(char*)] = {0};
    const char * prefix;
    if (trie)
@@ -85,12 +83,18 @@ trie_bsearch(const char *key,
              const size_t len,
              size_t * const index)
 {
-   struct trie *t = bsearch(key, list, len,
-                            sizeof(struct trie),
-                            trie_cmp);
-   if (t && index)
-      *index = t - list;
-   return !!t;
+   size_t s = 0, e = len, c;
+   int cmp;
+   while (c = (s+e)/2, cmp = trie_cmp(key, &list[c]), cmp && c > 0 && c < e)
+   {
+      if (cmp > 0)
+         s = c + 1;
+      else
+         e = c;
+   }
+   if (index)
+      *index = c;
+   return cmp == 0;
 }
 
 bool
